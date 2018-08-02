c8db8a96732e617a555afd3b8b7b106dcbc18a00
PascalF44 <pascal.fievre@ssx-solutions.fr>
2018-08-02 18:39:24 +0200

    Initialisation de la gestion du state du composant ListeVide

--- src/components/views/ListeVide.jsx
+++ src/components/views/ListeVide.jsx
@@ -1,6 +1,29 @@
 import React, {Component} from "react"
 
 class ListeVide extends Component{
+    constructor(props, context){
+        super(props, context);
+        this.state={
+            dateCourse: ""
+        };
+
+        this.dateCourse=this.dateCourse.bind(this);
+    }
+
+    dateCourse(e){
+        this.setState({
+            dateCourse: e.target.value
+        });
+    }
+
+    addListe(e){
+        this.setState({ 
+            dateCourse: this.state.dateCourse
+        });
+
+        e.preventDefault();
+    }
+
     render(){
         return(
             <div>
@@ -8,8 +31,8 @@ class ListeVide extends Component{
                     <span>Pas de liste en cours...</span>
                 </div>
                 <div className="liste-vide-form">
-                    <form>
-                        <input placeholder="pour quand ?" />
+                    <form onSubmit={this.addListe}>
+                        <input onChange={this.dateCourse} placeholder="pour quand ?" />
                         <button type="submit">Démarrer !</button>
                     </form>
                 </div>
